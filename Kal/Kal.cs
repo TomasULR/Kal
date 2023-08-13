14188175dda0701c5f1c51cf15556501200bcad6
tomas <tomas@DESKTOP-HQ49KJ7>
2023-08-13 18:44:01 +0200

    Add project files.

---
+++ Kal/Kal.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Kal;
+
+public class Kalc
+{
+    public int YearNow { get; set; } = DateTime.Now.Year;
+    public int MonthNow { get; set; } = DateTime.Now.Month;
+
+    string currentMonthName => new DateTime(YearNow, MonthNow, 1).ToString("MMMM", CultureInfo.InvariantCulture);
+    void PreviousMonth()
+    {
+        if (MonthNow == 1)
+        {
+            MonthNow = 12;
+            YearNow--;
+        }
+        else
+        {
+            MonthNow--;
+        }
+    }
+    void NextMonth()
+    {
+        if (MonthNow == 12)
+        {
+            MonthNow = 1;
+            YearNow++;
+        }
+        else
+        {
+            MonthNow++;
+        }
+    }
+}
